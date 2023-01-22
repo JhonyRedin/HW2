62cebe963c4dbd7bd73708233e34f2eadc567a6f
Georgy Redin <georgyredin@gmail.com>
2023-01-22 15:56:18 +0300

    Task 13 result

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+    number = number/100;
+    if (number == 0)
+    {
+        System.Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+    number = number%10;
+    System.Console.WriteLine($"Третья цифра: {number}");
+    }
